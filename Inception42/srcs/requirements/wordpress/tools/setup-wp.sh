c2ccfabc2d54c146ae5979e81d6d53e528dc6dd4
Julius de Baaij <julius@debaaij.com>
2025-02-04 18:41:53 +0100

    temporary changes

--- Inception42/srcs/requirements/wordpress/tools/setup-wp.sh
+++ Inception42/srcs/requirements/wordpress/tools/setup-wp.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
 
+set -e
+
+echo "Attempting to connect to database..."
+mysqladmin -h mariadb -u$MYSQL_USER -p$MYSQL_PASSWORD ping
+
 # Navigate to WordPress directory
 cd /var/www/html
 
-# If WordPress is not installed, download and configure
-if [ ! -f "wp-config.php" ]; then
-    # Download WordPress
-    wp core download --allow-root
+# Remove existing WordPress files
+rm -rf *
 
-    # Configure WordPress
-    wp config create \
-        --dbname=$MYSQL_DATABASE \
-        --dbuser=$MYSQL_USER \
-        --dbpass=$MYSQL_PASSWORD \
-        --dbhost=$WORDPRESS_DB_HOST \
-        --allow-root
+# Download WordPress
+wp core download --allow-root
 
-    # Install WordPress
-    wp core install \
-        --url=$WORDPRESS_URL \
-        --title="$WORDPRESS_TITLE" \
-        --admin_user=$WORDPRESS_ADMIN_USER \
-        --admin_password=$WORDPRESS_ADMIN_PASSWORD \
-        --admin_email=$WORDPRESS_ADMIN_EMAIL \
-        --allow-root
+# Configure WordPress with force flag
+wp config create \
+    --dbname=$MYSQL_DATABASE \
+    --dbuser=$MYSQL_USER \
+    --dbpass=$MYSQL_PASSWORD \
+    --dbhost=mariadb \
+    --allow-root \
+    --force
 
-    # Create a second user
-    wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL \
-        --role=editor \
-        --user_pass=$WORDPRESS_USER_PASSWORD \
-        --allow-root
-fi
+# Install WordPress
+wp core install \
+    --url=$WORDPRESS_URL \
+    --title="$WORDPRESS_TITLE" \
+    --admin_user=$WORDPRESS_ADMIN_USER \
+    --admin_password=$WORDPRESS_ADMIN_PASSWORD \
+    --admin_email=$WORDPRESS_ADMIN_EMAIL \
+    --allow-root
+
+# Create a second user
+wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL \
+    --role=editor \
+    --user_pass=$WORDPRESS_USER_PASSWORD \
+    --allow-root
 
 # Start PHP-FPM
 exec php-fpm7.3 -F
